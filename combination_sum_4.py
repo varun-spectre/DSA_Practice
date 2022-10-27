cfcb870bcfb3c6f173a846bc54e3f4d476cadc80
varun-spectre <31825973+varun-spectre@users.noreply.github.com>
2022-10-27 02:14:49 -0700

    Delete combination_sum_4.py

--- combination_sum_4.py
+++
@@ -1,8 +0,0 @@
-def combinationSum4(nums, target):
-    dp = {0:1} // "base case"
-    for i in range(1, target+1):
-        dp[i] = 0 // "we will start with zero and update the final value in the below loop"
-        for j in nums:
-            dp[i] += dp.get(i-j, 0) // "if we get negative values, we will return 0"
-        print(dp[i])
-    return dp[target] // "return dp[target] is nothing but sum(target - nums[0] , target - nums[2], ..., target - nums[n-1])"
